da3e3c101a18671b749a6f8143a0bb14f1c1d79a
Hashimoto <daniel.hashimoto@accenture.com>
2024-10-08 19:23:47 -0300

    feat: meu portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { CepComponent } from '../teste-tecnico/cep/containers/cep/cep.component';
+import { PortfolioComponent } from '../teste-tecnico/portfolio/components/portfolio/portfolio.component';
 import { HelloComponent } from './hello.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HelloComponent, MatExpansionModule, CepComponent],
+  imports: [CommonModule, HelloComponent, MatExpansionModule, CepComponent, PortfolioComponent, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
